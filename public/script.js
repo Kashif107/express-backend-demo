9c9ee6d7a4e703ec6e5508820de8a5c5d4cfa146
Kashif107 <mdkashifjaved731@gmail.com>
2025-07-26 23:17:17 +0530

    big changes in code added login page and calculator

--- public/script.js
+++
@@ -1,3 +0,0 @@
-function sayHi() {
-  alert("You clicked the button!");
-}
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
+// 👇 Serve main static site at "/"
+app.use(express.static(path.join(__dirname, "public")));
+
+// 👇 Serve calculator page at "/calculator"
+app.use("/calculator", express.static(path.join(__dirname, "calculator-site")));
 // ✅ Serve all files from "public" folder
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("🚀 Server is working on Render!");
+app.post('/api/login',(req,res)=>{
+    const {username ,password}=req.body;
+    if(username==="john" && password === "1234"){
+        console.log("login successful");
+        res.json({success:true ,message:"login successful", redirect: "/calculator-site"}); // Add redirect URL here
+    }
+    else{
+        console.log("login failed");
+        res.json({success:false ,message:"login failed"});
+    }
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
